4a3b3e7a086c679e3b4688b5aa29b0412d789a50
Anna Sokolova <sokolovaskorik@gmail.com>
2020-12-09 18:05:48 +0200

    Adds file for change theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -11,3 +11,30 @@ const Theme = {
 };
 // console.log(Theme);
 
+// функция для изменения темы при перетягивании ползунка(добавляет или удаляет класс с body и записывает текущую тему в LS)
+function handleChangeTheme(e) {
+  if (!e.target.checked) {
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
+    localStorage.setItem('setting', Theme.LIGHT);
+
+  } else {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('setting', Theme.DARK);
+  }
+}
+
+// функция для проверки состояния ползунка чекбокса и текущей темы
+function updateLS() {
+  if (localStorage.setting === 'dark-theme') {
+    body.classList.toggle(Theme.DARK);
+    btnCheck.checked = true;
+  } else {
+    body.classList.toggle(Theme.LIGHT);
+  }
+}
+
+updateLS(); //вызываем функцию для проверки текущей темы
+
+btnCheck.addEventListener('change', handleChangeTheme); //вешаем слушателя события на кнопку чекбокс
