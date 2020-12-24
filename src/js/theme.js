9700b81eca7c2c3fef9c36527e0b34d81f0c1259
Anna Sokolova <sokolovaskorik@gmail.com>
2020-12-24 10:05:28 +0200

    Adds the func for replace theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -14,25 +14,33 @@ const Theme = {
 // функция для изменения темы при перетягивании ползунка(добавляет или удаляет класс с body и записывает текущую тему в LS)
 function handleChangeTheme(e) {
   if (!e.target.checked) {
-    body.classList.add(Theme.LIGHT);
-    body.classList.remove(Theme.DARK);
-    localStorage.setItem('setting', Theme.LIGHT);
-
+    // body.classList.add(Theme.LIGHT);
+    // body.classList.remove(Theme.DARK);
+    // localStorage.setItem('setting', Theme.LIGHT);
+    replaceTheme(Theme.LIGHT, Theme.DARK);
   } else {
-    body.classList.add(Theme.DARK);
-    body.classList.remove(Theme.LIGHT);
-    localStorage.setItem('setting', Theme.DARK);
+    // body.classList.add(Theme.DARK);
+    // body.classList.remove(Theme.LIGHT);
+    // localStorage.setItem('setting', Theme.DARK);
+    replaceTheme(Theme.DARK, Theme.LIGHT);
   }
 }
 
 // функция для проверки состояния ползунка чекбокса и текущей темы
 function updateLS() {
-  if (localStorage.setting === 'dark-theme') {
+  if (localStorage.setting === Theme.DARK) {
     body.classList.toggle(Theme.DARK);
     btnCheck.checked = true;
   } else {
     body.classList.toggle(Theme.LIGHT);
   }
+}
+
+//функция для добавления или удаления темы
+function replaceTheme(oldTheme, newTheme) {
+  body.classList.add(newTheme);
+  body.classList.remove(oldTheme);
+  localStorage.setItem('setting', newTheme);
 }
 
 updateLS(); //вызываем функцию для проверки текущей темы
